9e321008d6f341cdb4c6ee8c1c9e2bd3b98487e7
Rayane Ait Ali Yahia <rayane.ait_ali_yahia@g.enp.edu.dz>
2022-03-23 23:45:25 +0100

    add initial embedding as parameter to train

--- train.py
+++ train.py
@@ -14,22 +14,9 @@ import seaborn as sns
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 from preprocessing import *
-from model import *
 from tqdm import tqdm
 from evaluate_classif import test_rodie
-
 from torch.utils.data import DataLoader
-def dynamic_embedding(data,embedding_dim):
-        num_users = len(torch.unique(data[:,0]))
-        num_items = len(torch.unique(data[:,1]))
-        dynamic_users_embedding = F.normalize(torch.rand(embedding_dim),dim=0).repeat(num_users,1)
-        dynamic_items_embedding = F.normalize(torch.rand(embedding_dim),dim=0).repeat(num_items+1,1)
-
-        print("Initialisation of dynamic embedding... Done !")
-        print("Dynamic Embedding shape : Users {}, \t Items {}".format(list(dynamic_users_embedding.size()),list(dynamic_items_embedding.size())))
-
-        return dynamic_users_embedding,dynamic_items_embedding
-
 
 
 def regularizer(actual_user_embedding,future_user_embedding,lambda_u,
@@ -56,13 +43,27 @@ def train_rodie(t_batches,
           ):
   losses_train = []
   losses_valid = []
+
+  initial_user_embedding = nn.Parameter(F.normalize(torch.rand(32).to(device), dim=0)) # the initial user and item embeddings are learned during training as well
+  initial_item_embedding = nn.Parameter(F.normalize(torch.rand(32).to(device), dim=0))
+  model.initial_user_embedding = initial_user_embedding
+  model.initial_item_embedding = initial_item_embedding
+  optimizer = torch.optim.Adam(model.parameters())
+  #for name, param in model.named_parameters():
+  #  if param.requires_grad:
+  #    print(name, param.data)
   print("Training...")
   for e in range(n_epochs):
+   # print(initial_item_embedding)
+   # print("\n")
+   # print("BEGIN ... \n")
+
+    U = initial_user_embedding.repeat(7047, 1) # initialize all users to the same embedding 
+    I = initial_item_embedding.repeat(98, 1) # initialize all items to the same embedding
+    U_copy, I_copy = U.detach().clone(), I.detach().clone()
     train_err = 0
-    U,I = dynamic_embedding(data,model.embedding_dim)  # Initial dynamic embedding
- # U_copy,I_copy = U.clone(), I.clone()
-    U = U.to(device)
-    I = I.to(device)
+    #print("END EPOCH : Users Embeddings after update EPOCH 2 \n")
+    #print(I)
     for (_,rows),_ in zip(t_batches.items(),tqdm(range(len(t_batches)), position=0, leave=True)):
       optimizer.zero_grad()
       users_idx,items_idx = extractItemUserId(data,rows)
@@ -94,9 +95,8 @@ def train_rodie(t_batches,
                 past_item_dynamic_embedding,
                 past_item_static_embedding)
       # Add the new embedding to the placeholder U and I
-      U[users_idx] = future_user_embedding.detach()
-      I[items_idx] = future_item_embedding.detach()
-      
+      U_copy[users_idx] = future_user_embedding.detach()
+      I_copy[items_idx] = future_item_embedding.detach()
       # Return loss value between the predicted embedding "j_tilde" and the real past item embedding j_true
       
       loss = MSELoss()(j_tilde,j_true)#.detach()
@@ -107,19 +107,22 @@ def train_rodie(t_batches,
       loss.backward()
       train_err += loss.item()
       optimizer.step()
+    #print("END EPOCH : EMbeddings after update \n")
+    #print(initial_item_embedding)
 
-    y, pred,_,_,auc,valid_err = test_rodie(valid_data,weight_ratio_valid,U, I, data, model, device)
+    y, pred,_,_,auc,valid_err = test_rodie(valid_data,weight_ratio_valid,U_copy, I_copy, data, model, device)
     losses_train.append(train_err/len(train_interactions))
     losses_valid.append(valid_err/(len(data)-len(train_interactions)))
 
     print("validation interactions {}".format(len(data)-len(train_interactions)))
-    print("Epoch {} Train Loss {}".format(e,losses_train[e]))
-    print("Epoch {} Validation Loss {} , AUC Score {}".format(e,losses_valid[e],auc))
+    print("Epoch {} Train Loss {}".format(e,train_err))
+    print("Epoch {} Validation Loss {} , AUC Score {}".format(e,valid_err,auc))
 
     if e%2 ==0:
       torch.save(model.state_dict(), "model_ep{}".format(e))
-
     print("Saving the model ...")
+
+
     torch.save(model.state_dict(), "modelFinal_ep{}".format(e))
-  return model,U,I,losses_train,losses_valid
+  return model,U_copy,I_copy,losses_train,losses_valid
 
